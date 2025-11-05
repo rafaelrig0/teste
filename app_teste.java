cc5553e51ac5fa40a2cb4d7bf57fa223f4fda11e
Rafael Rigo <rafaelrigoo13@gmail.com>
2025-11-05 15:59:11 -0300

    Create app_teste.java

---
+++ app_teste.java
@@ -0,0 +1,17 @@
+public class app_teste {
+
+    public static void main(String[] args) {
+        System.out.println("codigo externo");
+
+        int[] meus_numeros = { 10, 20, 30, 40, 50 };
+        int alvo = 30;
+
+        int resultado = Programa02.pesquisar(meus_numeros, alvo);
+
+        if (resultado != -1) {
+            System.out.println("Teste RAG:  posição: " + resultado);
+        } else {
+            System.out.println("Teste RAG: não encontrado.");
+        }
+    }
+}
